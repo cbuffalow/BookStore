b7d03fc1533de03a1664dcc84e3829a18dee7376
[zebulon0] <zah1276@rit.edu>
2019-12-09 17:37:26 -0500

    classes mostly setup with necessary attributes & methods.  Main file has one case implemented the rest still need to be finished to match the assignment description... AKA saving to a file and loading from a file

--- BookStoreODDRun.py
+++ BookStoreODDRun.py
@@ -1,24 +1,62 @@
-from BookStoreOODClass import Book
-
-cashbalance = 1000
-run = True
-
-while run:
-    mainmenu = input('''What would you like to do?
-                                    A. Add Book
-                                    S. Sell Book
-                                    R. Reduce book prices across the board by 2%
-                                    I. Increase book prices across the board by 2%
-                                    V. Display inventory
-                                    P. Save current inventory
-                                    O. Open and load an inventory
-                                    Q. Quit
-                                            :  ''')
-    if mainmenu.upper() == "A":
-        titleAdd = input("What is the title of the book you are adding: ")
-        authorAdd = input("What is the authors name: ")
-        costAdd = input("What is the price of this book: ")
-        Book.addbook(titleAdd, authorAdd, costAdd)
+from BookStoreOODClass import *
+# imports everything from the file
 
 
+def main():
+    cashbalance = 1000
+    CountryClubBookstore = BookStore(cashbalance)
+    # can also do BookStore(1000) since you're essentially hard coding it
 
+    while True:
+        mainmenu = input('''What would you like to do?
+                                        A. Add Book
+                                        S. Sell Book
+                                        R. Reduce book prices across the board by 2%
+                                        I. Increase book prices across the board by 2%
+                                        V. Display inventory
+                                        P. Save current inventory
+                                        O. Open and load an inventory
+                                        Q. Quit
+                                                :  ''')
+        # TODO
+        # do this for each case
+
+        if mainmenu.upper() == "A":
+            title = input("What is the title of the book you are adding: ")
+            author = input("What is the authors name: ")
+            cost = input("What is the price of this book: ")
+            CountryClubBookstore.add_book(title, author, cost)
+
+        elif mainmenu.upper() == "A":
+            None
+
+        elif mainmenu.upper() == "S":
+            None
+
+        elif mainmenu.upper() == "R":
+            None
+
+        elif mainmenu.upper() == "I":
+            None
+
+        elif mainmenu.upper() == "V":
+            None
+
+        elif mainmenu.upper() == "P":
+            None
+
+        elif mainmenu.upper() == "O":
+            None
+
+        elif mainmenu.upper() == "Q":
+            print("Goodbye...")
+            break
+
+        else:
+            print("Oops you must've entered an incorrect command")
+
+
+
+
+if __name__ == "__main__":
+    main()
--- BookStoreOODClass.py
+++ BookStoreOODClass.py
@@ -1,20 +1,40 @@
-class Book(object):
-    bookstore = titles, authors, prices = [], [], []
-
+class Book:
+    """Book is an object with a title, author, and a price"""
     def __init__(self, title, author, cost):
         self.title = title
         self.author = author
         self.cost = cost
 
-    def addbook(self, title, author, cost):
-        title.append(title)
-        author.append(author)
-        cost.append(int(cost))
+    def update_cost(self, new_cost):
+        self.cost = new_cost
 
 
+class BookStore:
+    """Bookstore is the main object that is created once and updated when necessary
+    i.e: buying / selling books, changing prices"""
+    books = []  # empty list
+    cash = 0    # starts at zero before init method is called
 
+    def __init__(self, seed_money):
+        self.cash = seed_money
 
+    def add_book(self, title, author, cost):
+        self.books.append(Book(title, author, cost))
 
+    def sell_book(self, book):  # takes in a book object to remove
+        cost = book.cost
+        self.books.remove(book)
+        # .remove only removes an object so it searches for the Book that matches
+        self.cash += cost  # increase cash because the book was sold
 
+    def increase_prices(self):
+        for i in range(len(self.books)):
+            temp = self.books.pop(i)  # remove at index
+            temp.update_cost(1.1*temp.cost)  # increase price of books by 10%
+            self.books.insert(i, temp)  # insert back at correct index
 
-
+    def decrease_prices(self):
+        for i in range(len(self.books)):
+            temp = self.books.pop(i)  # remove at index
+            temp.update_cost(.9*temp.cost)  # decrease price of books by 10%
+            self.books.insert(i, temp)  # insert back at correct index
