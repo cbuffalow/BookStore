726b6ed22589d5863cb9d998cc2d444624ee19db
cbuffalow <cbuffalow@ycp.edu>
2019-12-09 20:17:01 -0500

    OOP programming version  of bookstore

--- BookStoreOODClass.py
+++ BookStoreOODClass.py
@@ -1,16 +1,29 @@
-class Book(object):
-    bookstore = titles, authors, prices = [], [], []
+class BookStore:
 
-    def __init__(self, title, author, cost):
-        self.title = title
-        self.author = author
-        self.cost = cost
+    def __init__(self):
+        self.title = ""
+        self.author = ""
+        self.cost = ""
+        self.bookstore = []
+        self.bookstore = self.title, self.author, self.cost = [], [], []
+        self.books = []
+        self.cash = 1000
+
+    def update_cost(self, new_cost):
+        self.cost = new_cost
 
     def addbook(self, title, author, cost):
-        title.append(title)
-        author.append(author)
-        cost.append(int(cost))
+        self.title.append(title)
+        self.author.append(author)
+        self.cost.append(cost)
+        print(self.bookstore)
 
+    def sellbook(self, Book):
+        for i in self.bookstore:
+            i.remove(Book)
+
+    def inventory(self):
+        print(self.bookstore)
 
 
 
